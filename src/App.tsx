89c4dc030c457a6f5f0901f5b901813bd8b7e20e
demonze.warren <demonze.warren@outlook.com>
2023-09-06 14:25:59 -0500

    Merge remote-tracking branch 'origin/main'

--- src/App.tsx
+++ src/App.tsx
@@ -2,60 +2,48 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
-/**
- * State declaration for <App />
- */
+
 interface IState {
-  data: ServerRespond[],
-  showGraph: boolean,
+  data: ServerRespond[];
+  showGraph: boolean;
 }
 
-/**
-@@ -22,26 +23,40 @@ class App extends Component<{}, IState> {
-      // data saves the server responds.
-      // We use this state to parse data down to the child element (Graph) as element property
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
       data: [],
       showGraph: false,
     };
   }
 
-  /**
-   * Render Graph react component with state.data parse as property data
-   */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
-      if(this.state.showGraph) {
-        return (<Graph data={this.state.data}/>)
-      }
+    // Render the Graph component only if showGraph is true
+    if (this.state.showGraph) {
+      return <Graph data={this.state.data} />;
+    }
   }
 
-  /**
-   * Get new data from server and update the state with the new data
-   */
   getDataFromServer() {
     let x = 0;
     const interval = setInterval(() => {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-      // this.setState({ data: [...this.state.data, ...serverResponds] });
-      this.setState ({
-        data: serverResponds,
-        showGraph: true,
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from the server
+        this.setState({
+          data: [...this.state.data, ...serverResponds],
+          showGraph: true, // Set showGraph to true to render the Graph component
+        });
       });
-    });
-  }
-    x++;
-    if(x > 1000){
-      clearInterval(interval)
-    }
-  }, 100);
-}
 
-  /**
-   * Render the App react component
-   */
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+
   render() {
     return (
       <div className="App">
@@ -63,13 +51,12 @@ interface IState {
           Bank & Merge Co Task 2
         </header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
+          <button
+            className="btn btn-primary Stream-button"
+            onClick={() => {
+              this.getDataFromServer();
+            }}
+          >
             Start Streaming Data
           </button>
           <div className="Graph">
@@ -77,7 +64,8 @@ interface IState {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
+
 export default App;
