27a90bcf10a0ece09326a49149a4093366c784ee
demonze.warren <demonze.warren@outlook.com>
2023-09-06 12:20:06 -0500

    Merge remote-tracking branch 'origin/main'

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,93 +1,64 @@
 import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
 import './Graph.css';
 
-/**
- * Props declaration for <Graph />
- */
 interface IProps {
-  data: ServerRespond[];
+  data: ServerRespond[],
 }
 
-/**
- * Perspective library adds load to HTMLElement prototype.
- * This interface acts as a wrapper for Typescript compiler.
- */
 interface PerspectiveViewerElement extends HTMLElement {
-  load: (table: Table) => void;
+  load: (table: Table) => void,
 }
-
-/**
- * React component that renders Perspective based on data
- * parsed from its parent through the data property.
- */
 class Graph extends Component<IProps, {}> {
-  // Perspective table
   table: Table | undefined;
 
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
   componentDidMount() {
-    // Get the element to attach the table from the DOM.
+    // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float'
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
-
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        "stock": "distinct count",
-        "top_ask_price": "avg",
-        "top_bid_price": "avg",
-        "timestamp": "distinct count",
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio:'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
-  componentDidUpdate(prevProps: IProps) {
-    // Every time the data props is updated, insert the data into Perspective table
-    if (this.table && this.props.data !== prevProps.data) {
-      // Define a type for the table data
-type TableData = {
-  stock: string;
-  top_ask_price: number;
-  top_bid_price: number;
-  timestamp: Date;
-}[];
-
-// ...
-
-// Update the type of `formattedData`
-const formattedData: TableData = this.props.data.map((el: ServerRespond) => ({
-  stock: el.stock,
-  top_ask_price: el.top_ask?.price || 0,
-  top_bid_price: el.top_bid?.price || 0,
-  timestamp: el.timestamp,
-}));
-
-// Update the table with `formattedData`
-this.table.update(formattedData);
-
+  componentDidUpdate() {
+    if (this.table) {
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),
+      ]);
     }
-  }
-
-  render() {
-    return <perspective-viewer />;
   }
 }
 
